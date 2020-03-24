ee4fa2a02117bf2a254ad81ecedc51ba36271850
William Bigas Mauro <williambmauro@hotmail.com>
2020-03-24 16:47:40 -0300

    Adicionado TextView dinamico quando verifica a idade

--- app/src/main/java/br/com/strixteam/view/MainActivity.java
+++ app/src/main/java/br/com/strixteam/view/MainActivity.java
@@ -7,8 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import br.com.strixteam.R;
 import br.com.strixteam.model.Pessoa;
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText editIdade;
     private TextView tvResultado;
     private ImageView ivResultado;
+    private LinearLayout layoutRes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         tvResultado = findViewById(R.id.tvResultado);
         btnFechar = findViewById(R.id.btnFechar);
         ivResultado = findViewById(R.id.ivResultado);
+        layoutRes = findViewById(R.id.layoutRes);
     }
 
     public void btnFecharListener() {
@@ -73,13 +78,23 @@ public class MainActivity extends AppCompatActivity {
             editIdade.setError(getString(R.string.erro_idade));
         } else {
 
-            tvResultado.setText("Olá " + pessoa.getNome() + ", você possui " + pessoa.getIdade() + " ano(s)");
-
+            // COLOCANDO IMAGEM EM FORMA ESTATICA
             if (PessoaBO.verificarMaioridadeIdade(pessoa.getIdade())) {
                 ivResultado.setImageResource(R.drawable.maior_idade);
             } else {
                 ivResultado.setImageResource(R.drawable.menor_idade);
             }
+
+
+            // Formato Estatico
+            tvResultado.setText("Olá " + pessoa.getNome() + ", você possui " + pessoa.getIdade() + " ano(s)");
+
+//            // Formato Dinâmico
+//            TextView tvResDinamico = new TextView(this);
+//            tvResDinamico.setText("###Olá " + pessoa.getNome() + ", você possui " + pessoa.getIdade() + " ano(s)");
+//            layoutRes.addView(tvResDinamico);
+
+
 
         }
 
